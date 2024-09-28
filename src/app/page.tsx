a99fd1b90555f676bfaa3f95fc181d9894ce81fb
norovgo <norov1.android@yandex.ru>
2024-09-28 16:26:22 +0300

    chore(1): add next-core

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/shared/ui/button";
+
+export default function Home() {
+  return (
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Button>Bitton</Button>
+      </main>
+  );
+}
