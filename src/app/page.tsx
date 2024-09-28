66a8e0798ec9bbe3e0f3ff66e708714ab008c0e9
norovgo <norov1.android@yandex.ru>
2024-09-28 22:58:31 +0300

    check deploy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-screen flex-col p-8">
-      <h1>Courses!</h1>
+      <h1>Courses</h1>
       <CreateCourseForm revalidatePagePath="/" className="max-w-[300px] mb-10"/>
       <CoursesList revalidatePagePath="/"/>
     </main>
